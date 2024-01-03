0712ba00ec5d5df5906e28fc57932e7a8aa91e9e
Diego-Gomez17 <diego.gomez.collado@gmail.com>
2024-01-03 11:48:10 -0400

    feat(EditView): button for delete has been added to view

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -3,13 +3,14 @@
     <h2>Editar Registro</h2>
     <div class="forms">
       <h3>Buscar Pago</h3>
-      <form action="" class="d-flex flex-column gap-3">
-        <label for="">Rut del apoderado</label>
+      <div class="d-flex flex-column gap-3">
+        <label for=""><b>Rut del apoderado</b></label>
         <input type="text" v-model="rutInput" required />
         <button type="button" @click="searchPay" class="btn btn-warning">
           Buscar
         </button>
-      </form>
+      </div>
+
       <table class="table">
         <thead>
           <tr>
@@ -25,13 +26,11 @@
             <th scope="col">Fecha de pago</th>
             <th scope="col">Modo de pago</th>
             <th scope="col">Administrador</th>
+            <th scope="col">Accion</th>
           </tr>
         </thead>
         <tbody>
-          <tr
-            v-for="(payClient, index) in paginatedPayments"
-            :key="payClient.id"
-          >
+          <tr v-for="(payClient, index) in userOptions" :key="payClient.id">
             <th scope="row">{{ index + 1 }}</th>
             <td>{{ payClient.apoderado.rut }}</td>
             <td>{{ payClient.apoderado.nombre }}</td>
@@ -44,6 +43,11 @@
             <td>{{ payClient.fechaPago }}</td>
             <td>{{ payClient.modoPago }}</td>
             <td>{{ payClient.admin }}</td>
+            <td>
+              <button @click="deletePay(payClient)" class="btn btn-danger">
+                x
+              </button>
+            </td>
           </tr>
         </tbody>
       </table>
@@ -52,32 +56,56 @@
 </template>
 <script setup>
 import { ref as refVue, onMounted } from "vue";
-import { ref, push, onValue } from "firebase/database";
+import { ref, push, onValue, child, remove } from "firebase/database";
 import { db } from "../Firebase/init";
 
-const apoderadoRef = ref(db, "pagos");
-const userOptions = refVue([]);
-const rutInput = refVue();
-const listPay= []
-function searchPay() {
-  // Realiza la búsqueda del apoderado por el rut
-  const searchRut = rutInput.value 
-  if(searchRut != null)
-  {
+const payRef = ref(db, "pagos");
 
-      onValue(apoderadoRef, (snapshot)=>{
-          
-          const data = snapshot.val()
-          for (const key in data ){
-              if( data[key].apoderado.rut === searchRut){
-                  listPay.push(data[key])
-                  console.log(listPay)
-                  
-                }
-            }
-        })
+const rutInput = refVue();
+const userOptions = refVue([]);
+function searchPay() {
+  const listPay = [];
+  // Realiza la búsqueda del apoderado por el rut
+  const searchRut = rutInput.value;
+  if (searchRut != null) {
+    onValue(payRef, (snapshot) => {
+      const data = snapshot.val();
+      for (const key in data) {
+        if (data[key].apoderado.rut === searchRut) {
+          listPay.push({
+            id: key,
+            ...data[key],
+          });
+        }
+      }
+    });
+  }
+  userOptions.value = listPay;
+  console.log(userOptions.value);
+}
+
+function deletePay(pay) {
+  Swal.fire({
+    title: "Estas seguro de eliminar este registro?",
+    text: "¡No podrás revertir esto!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    cancelButtonText: "Cancelar",
+    confirmButtonText: "Confirmar",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      remove(ref(db, `pagos/${pay.id}/`));
+      console.log("se ha eliminado el documento");
+      searchPay();
+      Swal.fire({
+        title: "Eliminado",
+        text: "El registro se ha eliminado con exito",
+        icon: "success",
+      });
     }
-    userOptions = listPay
+  });
 }
 </script>
 <style>
@@ -94,6 +122,17 @@ function searchPay() {
   border-radius: 15px;
   background-color: white;
   padding: 15px;
-  width: 65vw;
+  width: 85vw;
+}
+h3{
+    text-align: center;
+  }
+@media (max-width: 1300px) {
+  .forms{
+    font-size: 12px;
+    min-width: 1000px;
+
+  }
+  
 }
 </style>
